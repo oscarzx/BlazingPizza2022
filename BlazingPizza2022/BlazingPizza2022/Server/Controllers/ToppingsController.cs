8711285f47da35fa8a001a7e0ff50ac4fc58de73
Oscar Espinosa <oscarzx@gmail.com>
2022-11-29 15:29:03 -0500

    Adición toppings

---
+++ BlazingPizza2022/BlazingPizza2022/Server/Controllers/ToppingsController.cs
@@ -0,0 +1,27 @@
+﻿using BlazingPizza2022.Server.Models;
+using BlazingPizza2022.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza2022.Server.Controllers
+{
+    [Route("toppings")]
+    [ApiController]
+    public class ToppingsController : ControllerBase
+    {
+        private readonly PizzaStoreContext _context;
+
+        public ToppingsController(PizzaStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Topping>>> GetToppings()
+        {
+            return await _context.Toppings
+                .OrderBy(t => t.Name).ToListAsync();
+        }
+    }
+}
